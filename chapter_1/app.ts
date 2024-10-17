cea24efa64463864a403662a36b2afec716ddcce
_yeriel <yeriel37222@gmail.com>
2024-10-17 14:21:41 +0900

    TS : 믹스인 기법을 사용한 extend

--- chapter_1/app.ts
+++ chapter_1/app.ts
@@ -43,34 +43,45 @@ const store: Store = {
   feeds: [],
 }
 
+function applyApiMixins(targetClass: any, baseClasses: any[]): void {
+  baseClasses.forEach(baseClass => {
+    Object.getOwnPropertyNames(baseClass.prototype).forEach(name => {
+      const descriptor = Object.getOwnPropertyDescriptor(baseClass.prototype, name);
+
+      if(descriptor) {
+        Object.defineProperty(targetClass.prototype, name, descriptor)
+      }
+    });
+  });
+}
+
 class Api {
-  url: string;
-  ajax: XMLHttpRequest;
-  constructor(url: string) {
-    this.url = url;
-    this.ajax = new XMLHttpRequest();
-  }
-
-  // protected
-  protected getRequest<AjaxResponse>(): AjaxResponse {
-    this.ajax.open('GET', this.url, false);
-    this.ajax.send();
+  getRequest<AjaxResponse>(url: string): AjaxResponse {
+    const ajax = new XMLHttpRequest();
+    ajax.open('GET', url, false);
+    ajax.send();
   
-    return JSON.parse(this.ajax.response);    
+    return JSON.parse(ajax.response);    
   }
 }
 
-class NewsFeedApi extends Api {
+class NewsFeedApi {
   getData(): NewsFeed[] {
-    return this.getRequest<NewsFeed[]>();    
+    return this.getRequest<NewsFeed[]>(NEWS_URL);    
   }
 }
 
-class NewsDetailApi extends Api {
-  getData(): NewsDetail {
-    return this.getRequest<NewsDetail>();
+class NewsDetailApi {
+  getData(id: string): NewsDetail {
+    return this.getRequest<NewsDetail>(CONTENT_URL.replace("@id", id));
   }
 }
+
+interface NewsFeedApi extends Api {};
+interface NewsDetailApi extends Api {};
+
+applyApiMixins(NewsFeedApi, [Api]);
+applyApiMixins(NewsDetailApi, [Api]);
 
 // getData<AjaxResponse>(url: string): AjaxResponse  = getData(url: string): NewsFeed[] | NewsDetail
 function getData<AjaxResponse>(url: string): AjaxResponse {
@@ -99,7 +110,7 @@ function updateView(html: string): void {
 }
 
 function newsFeed(): void {
-  const api = new NewsFeedApi(NEWS_URL);
+  const api = new NewsFeedApi();
   let newsFeed: NewsFeed[] = store.feeds;
   const newsList: string[] = [];
   let template = `
@@ -160,8 +171,8 @@ function newsFeed(): void {
 
 function newsDetail() {
   const id = location.hash.substring(7);
-  const api = new NewsDetailApi(CONTENT_URL.replace("@id", id));
-  const newsConts = api.getData();
+  const api = new NewsDetailApi();
+  const newsDetail: NewsDetail = api.getData(id);
   let tempalte = `
     <div class="bg-gray-600 min-h-screen pb-0">
       <div class="bg-white text-xl">
@@ -178,9 +189,9 @@ function newsDetail() {
       </div>
       
       <div class="h-full border rounded-xl bg-white m-6 p-4">
-        <h2>${newsConts.title}</h2>
+        <h2>${newsDetail.title}</h2>
         <div class="text-gray-400">
-          ${newsConts.content}
+          ${newsDetail.content}
         </div>
         {{__comments__}}
       </div>      
@@ -194,7 +205,7 @@ function newsDetail() {
     }
   }
 
-  updateView(tempalte.replace('{{__comments__}}', makeComment(newsConts.comments)));
+  updateView(tempalte.replace('{{__comments__}}', makeComment(newsDetail.comments)));
 }
 
 function makeComment(comments: NewsComment[]): string {
