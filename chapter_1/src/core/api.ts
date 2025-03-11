937a047ee56ba755d5e8d6d814f93babf090d6ee
_yeriel <yeriel37222@gmail.com>
2025-03-11 17:42:56 +0900

    no message

---
+++ chapter_1/src/core/api.ts
@@ -0,0 +1,39 @@
+import {NewsFeed, NewsDetail} from '../types/';
+
+export class Api {
+  ajax: XMLHttpRequest;
+  url: string;
+
+  constructor(url: string) {
+    this.ajax = new XMLHttpRequest();
+    this.url = url;
+  }
+
+  getRequest<AjaxResponse>(cb: (data: AjaxResponse) => void): void {
+    this.ajax.open('GET', this.url);
+    this.ajax.addEventListener('load', () => {
+      cb(JSON.parse(this.ajax.response) as AjaxResponse);
+    });
+    this.ajax.send();
+  }
+}
+
+export class NewsFeedApi extends Api {
+  constructor(url: string){
+    super(url);
+  }
+
+  getData(cb: (data: NewsFeed[]) => void): void {
+    return this.getRequest<NewsFeed[]>(cb);
+  }
+}
+
+export class NewsDetailApi extends Api {
+  constructor(url: string) {
+    super(url);
+  }
+
+  getData(cb: (data: NewsDetail) => void): void {
+    return this.getRequest<NewsDetail>(cb);
+  }
+}
